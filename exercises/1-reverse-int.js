605ed41ecfd32a5751c8628f39a7baa0811985c2
ilonaGold <songa17@inbox.lv>
2025-02-18 18:12:07 +0200

    feat: implement function to reverse integers

--- exercises/1-reverse-int.js
+++ exercises/1-reverse-int.js
@@ -8,4 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n.toString().split("").reverse().join("");
+  return parseInt(reversed) * Math.sign(n);
+}
+
+console.log(reverseInt(51));
+console.log(reverseInt(189));
+console.log(reverseInt(500));
+console.log(reverseInt(-15));
+console.log(reverseInt(-9));
+
+// Math.sign() returns 1 if the number is positive, -1 if the number is negative, and 0 if the number is 0.
