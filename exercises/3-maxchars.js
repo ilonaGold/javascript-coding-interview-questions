f5091a149e00ef7bd5368bf82f7ba940dfa63cea
ilonaGold <songa17@inbox.lv>
2025-02-18 19:36:30 +0200

    feat: add a solution for finding max character in an object

--- exercises/3-maxchars.js
+++ exercises/3-maxchars.js
@@ -5,4 +5,59 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  // for (let char of str) {
+  //   if (charMap[char]) {
+  //     charMap[char]++;
+  //   } else {
+  //     charMap[char] = 1;
+  //   }
+  // }
+
+  for (let char of str) {
+    // charMap[char] = charMap[char] + 1 || 1;
+    charMap[char] = ++charMap[char] || 1;
+  }
+
+  // for (const [key, value] of Object.entries(charMap)) {
+  //   // converts an object to an array
+  //   // console.log(key, value);
+  //   if (value > max) {
+  //     max = value;
+  //     maxChar = key;
+  //   }
+  // }
+
+  // newer way of looping through an object:
+  for (let key in charMap) {
+    if (charMap[key] > max) {
+      max = charMap[key];
+      maxChar = key;
+    }
+  }
+
+  // return charMap; // { a: 1, b: 1, c: 7, d: 1 }
+  return maxChar;
+
+  // OR
+  // for (let char of str) {
+  //   charMap[char] = charMap[char] + 1 || 1;
+  // }
+
+  // for (let char in charMap) {
+  //   if (charMap[char] > max) {
+  //     max = charMap[char];
+  //     maxChar = char;
+  //   }
+  // }
+
+  // return maxChar;
+}
+
+console.log(maxChar("abcccccccd"));
+
+// The technique is also useful to solve anagrams (does sting A has the same character as string B) or detect ifthe given string has any repeated characters.
