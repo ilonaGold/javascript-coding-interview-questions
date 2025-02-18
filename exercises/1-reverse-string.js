495100f4e8d99585290204dfb52f70e0b227ef72
ilonaGold <songa17@inbox.lv>
2025-02-18 17:55:13 +0200

    feat: implement reverse string function

--- exercises/1-reverse-string.js
+++ exercises/1-reverse-string.js
@@ -5,4 +5,37 @@
 //   reverse('hello') === 'olleh'
 //   reverse('CodingMoney') === 'yenoMgnidoC'
 
-function reverse(str) {}
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// Solution 1 - Using for loop
+
+function reverse(str) {
+  let reversed = "";
+  for (let i = 0; i < str.length; i++) {
+    // reversed = str[i] + reversed;
+    reversed += str[i]; // add the character to the beginning of the reversed string
+  }
+  return reversed;
+}
+
+// Solution 2 - Using for...of loop
+
+// function reverse(str) {
+//   let reversed = "";
+//   for (let char of str) {
+//     reversed += char;
+//   }
+//   return reversed;
+// }
+
+// Solution 3 - Using split, reverse and join methods
+
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+console.log(reverse("hi"));
+console.log(reverse("hello"));
+console.log(reverse("CodingMoney"));
