592c08f4439f1f0d5b4b7b13f912a7758fe8c590
ilonaGold <songa17@inbox.lv>
2025-02-18 18:29:26 +0200

    feat: add polindrome soltuion

--- exercises/2-palindrome.js
+++ exercises/2-palindrome.js
@@ -1,12 +1,37 @@
 // --- Directions
 // Given a string, return true if the string is a palindrome
 // or false if it is not.  Palindromes are strings that
-// form the same word if it is reversed. 
+// form the same word if it is reversed.
 
 // --- Examples:
 //   palindrome("kayak") === true
 //   palindrome("madam") === true
 //   palindrome("codingmoney") === false
 
-function palindrome(str) {}
+// Solution 1 - with if...else statement
 
+function palindrome(str) {
+  const reversed = str.split("").reverse().join("");
+  if (reversed === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Solution 2 - with return statement
+
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join("");
+//   return reversed === str;
+
+// Solution 3 - with ternary operator
+
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join("");
+//   return reversed === str ? true : false;
+// }
+
+console.log(palindrome("kayak"));
+console.log(palindrome("madam"));
+console.log(palindrome("codingmoney"));
